c674c541bc6db2a63157cf87034eadd2287c01c4
CORP\andmra2 <andrej.mrak2@hidria.com>
2022-01-30 13:25:33 +0100

    Task model added.

--- ToDoManager/Tasks/models.py
+++ ToDoManager/Tasks/models.py
@@ -1,3 +1,71 @@
 from django.db import models
+from django.db.models.fields import TextField
+from rest_framework.fields import CharField
+from django.core.validators import MaxValueValidator, MinValueValidator
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import User
 
-# Create your models here.
+from django.utils.translation import gettext_lazy as _
+
+
+class Task(models.Model):
+
+    created_by = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.CASCADE,
+    )
+
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+    )
+
+    updated_at = models.DateTimeField(
+        auto_now=True,
+    )
+
+    active = models.BooleanField(
+        default=True,
+    )
+
+    name = models.CharField(
+        _('Name'),
+        max_length=255,
+    )
+
+    details = models.TextField(
+        _('Details')
+    )
+
+    responsible = models.ForeignKey(
+        User(),
+        on_delete=models.CASCADE,
+    )
+
+    next_occurrence = models.DateTimeField(
+        _('Next Occurrence'),
+    )
+
+    due = models.DurationField(
+        _('Due'),
+    )
+
+    urgency = models.PositiveSmallIntegerField(
+        _('Urgency'),
+        default=1,
+        validators=[
+            MaxValueValidator(100),
+            MinValueValidator(1)
+        ]
+    )
+
+    planned_duration = models.DurationField(
+        _('Planned duration'),
+        null=True,
+        blank=True,
+    )
+
+    recurrence = models.DurationField(
+        _('Next Occurrence'),
+        null=True,
+        blank=True,
+    )
