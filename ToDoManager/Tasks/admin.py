af1058446f4566bf8fd380f7266743f51a52d255
CORP\andmra2 <andrej.mrak2@hidria.com>
2022-01-31 13:49:56 +0100

    Tasks app admins added.

--- ToDoManager/Tasks/admin.py
+++ ToDoManager/Tasks/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from Tasks.models import Task, Subtask, TaskOccurrence, SubtaskOccurrence
 
-# Register your models here.
+
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ['name', 'responsible', 'next_occurrence', 'active']
+    list_editable = ['active']
+
+class SubtaskAdmin(admin.ModelAdmin):
+    list_display = ['name', 'task', 'subtask_group', 'average_duration', 'active']
+    list_editable = ['active']
+
+class TaskOccurrenceAdmin(admin.ModelAdmin):
+    list_display = ['task', 'duration', 'completed']
+    list_editable = ['completed']
+
+class SubtaskOccurrenceAdmin(admin.ModelAdmin):
+    list_display = ['task_occurrence', 'duration', 'completed']
+    list_editable = ['completed']
+
+admin.site.register(Task, TaskAdmin)
+admin.site.register(Subtask, SubtaskAdmin)
+admin.site.register(TaskOccurrence, TaskOccurrenceAdmin)
+admin.site.register(SubtaskOccurrence, SubtaskOccurrenceAdmin)
